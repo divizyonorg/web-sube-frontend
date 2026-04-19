a99f741e9b72587bbf946c95faf8c6ef246479da
Samet Geçgel <91159031+samet-gecgel@users.noreply.github.com>
2026-04-19 16:29:11 +0300

    Revert "Dockercomposeyeni"

--- ViewModels/ClientSummaryViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace MyApp.Web.ViewModels;
-
-public class ClientSummaryViewModel
-{
-    public int TotalCount { get; set; }
-    public IReadOnlyList<string> RecentActiveNames { get; set; } = [];
-}
