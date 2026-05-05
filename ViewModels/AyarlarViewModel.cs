72d5b972e99a705cf1d276f19d1241be7d2a744c
Mustafa Çakır <enescakir31487@gmail.com>
2026-05-05 20:59:59 +0300

    guvenlik sayfasi tamamlandi

--- ViewModels/AyarlarViewModel.cs
+++ ViewModels/AyarlarViewModel.cs
@@ -3,4 +3,5 @@ namespace MyApp.Web.ViewModels;
 public class AyarlarViewModel
 {
     public string ActiveTab { get; set; } = "finansal-profil";
+    public string OtpPhone  { get; set; } = "0532 123 45 67";
 }
